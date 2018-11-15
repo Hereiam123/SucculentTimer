5feaefac6d594601e489775453f743304f28c0d1
Brian De Maio <bdemaio1@gmail.com>
2018-11-14 20:27:04 -0500

    More database implementation

--- app/src/main/java/com/example/briandemaio/succulenttimer/SucculentDao.java
+++ app/src/main/java/com/example/briandemaio/succulenttimer/SucculentDao.java
@@ -1,4 +1,20 @@
 package com.example.briandemaio.succulenttimer;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface SucculentDao {
+    @Insert
+    void insert(Succulent succulent);
+
+    @Query("DELETE FROM succulent_table")
+    void deleteAll();
+
+    @Query("SELECT * from succulent_table ORDER BY succulent ASC")
+    LiveData<List<Succulent>> getAllSucculents();
 }
---
+++ app/src/main/java/com/example/briandemaio/succulenttimer/SucculentRepository.java
@@ -0,0 +1,41 @@
+package com.example.briandemaio.succulenttimer;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class SucculentRepository {
+    private SucculentDao mSucculentDao;
+
+    private LiveData<List<Succulent>> mAllSucculents;
+    SucculentRepository(Application application) {
+        SucculentRoomDatabase db = SucculentRoomDatabase.getDatabase(application);
+        mSucculentDao = db.succulentDao();
+        mAllSucculents = mSucculentDao.getAllSucculents();
+    }
+
+    LiveData<List<Succulent>> getAllSucculents() {
+        return mAllSucculents;
+    }
+
+    public void insert (Succulent succulent) {
+        new insertAsyncTask(mSucculentDao).execute(succulent);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Succulent, Void, Void> {
+
+        private SucculentDao mAsyncTaskDao;
+
+        insertAsyncTask(SucculentDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Succulent... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
